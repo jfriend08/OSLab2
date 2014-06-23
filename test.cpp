50e7398f70976c85332caf66df2ce43df11fa250
jfriend08 <ys486@cornell.edu>
2014-06-23 12:47:18 -0400

    First step with randNum generator

---
+++ test.cpp
@@ -0,0 +1,41 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector> 
+#include <list>
+#include <sstream>
+#include <iterator>
+#include <map> 
+#include <iomanip>
+#include <utility> 
+#include <stdlib.h>
+
+using namespace std;
+
+vector<int> randvals;
+int ofs=0;
+int current_burst=10;
+
+int myrandom(int burst, int &index) { 	
+	if (index==randvals.size()){index=1;}
+	index++;		
+	return 1 + (randvals[index] % burst);	
+}
+
+int main(int argc, char *argv[]){
+	ifstream fin ( argv[1] ); // processing the rand file
+	if (!fin.is_open()){
+		cout<<"Cannot open the file"<<endl;}
+	else{
+		for (int i=0; !fin.eof();i++){
+			int temp;
+			fin>>temp;
+			randvals.push_back(temp);
+		}
+	}
+
+	// test for the rand number generator
+	for (int i=0; i<10; i++){		
+		cout<<myrandom(current_burst, ofs)<<endl;
+	}
+}
